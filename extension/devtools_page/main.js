06d995e46962a4e5965196b36c6533f889f11530
ginpei <ginpei@gmail.com>
2018-06-14 11:46:00 -0700

    show z-index ranking for Firefox

--- extension/devtools_page/main.js
+++ extension/devtools_page/main.js
@@ -11,16 +11,43 @@
     });
   }
 
+  // fore Firefox
+  // (Chrome shows z-index/index.html instead)
+  function updateTable (pane) {
+    const src = `
+      Array.from(document.querySelectorAll('*'))
+        .reduce((map, el) => {
+          const { zIndex } = getComputedStyle(el);
+          if (isNaN(zIndex)) {
+            return map;
+          }
+
+          if (!(zIndex in map)) {
+            map[zIndex] = [];
+          }
+          map[zIndex].push(el);
+          return map;
+        }, Object.create(null));
+    `;
+    pane.setExpression(src, 'z-index');
+  }
+
   async function start () {
     const pane = await createSidebarPane('z-index');
-    pane.setPage('/devtools_page/panes/z-index.html');
+    if (pane.setPage) {
+      // Chrome has `setPage()` 🙂
+      pane.setPage('/devtools_page/panes/z-index.html');
 
-    pane.onShown.addListener(() => sendMessage('updateTable'));
-    pane.onHidden.addListener(() => sendMessage('clearTable'));
+      pane.onShown.addListener(() => sendMessage('updateTable'));
+      pane.onHidden.addListener(() => sendMessage('clearTable'));
 
-    browser.devtools.panels.elements.onSelectionChanged.addListener(() => {
-      sendMessage('updateTable');
-    });
+      browser.devtools.panels.elements.onSelectionChanged.addListener(() => {
+        sendMessage('updateTable');
+      });
+    } else {
+      // Firefox does not have `setPage()` so... 😢
+      pane.onShown.addListener(() => updateTable(pane));
+    }
   }
 
   start();
